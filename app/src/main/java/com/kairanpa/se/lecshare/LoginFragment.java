5e80ba73f1e3506de48133864db954bbb01e6663
Skyzwing <59070180@it.kmitl.ac.th>
2018-10-12 02:19:33 +0700

    Register page update and add LoginFragemnt, fragment_login is blank page.

---
+++ app/src/main/java/com/kairanpa/se/lecshare/LoginFragment.java
@@ -0,0 +1,24 @@
+package com.kairanpa.se.lecshare;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginFragment extends Fragment {
+    private FirebaseAuth fbAuth;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
--- app/src/main/java/com/kairanpa/se/lecshare/RegisterFragment.java
+++ app/src/main/java/com/kairanpa/se/lecshare/RegisterFragment.java
@@ -4,11 +4,21 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterFragment extends Fragment {
+    private FirebaseAuth fbAuth;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -18,5 +28,64 @@ public class RegisterFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        fbAuth = FirebaseAuth.getInstance();
+        initRegister();
+    }
+
+    private void initRegister(){
+        Button _regisBtn = getView().findViewById(R.id.registerBtn);
+        _regisBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText _username = getView().findViewById(R.id.username);
+                EditText _email = getView().findViewById(R.id.email);
+                EditText _stdNumber = getView().findViewById(R.id.stdNumber);
+                EditText _passowrd = getView().findViewById(R.id.password);
+                EditText _rePassword = getView().findViewById(R.id.rePassword);
+
+                String _usernameStr = _username.getText().toString();
+                String _emailStr = _email.getText().toString();
+                String _stdNumberStr = _stdNumber.getText().toString();
+                String _passwordStr = _passowrd.getText().toString();
+                String _repasswordStr = _rePassword.getText().toString();
+
+                if(_usernameStr.isEmpty() || _emailStr.isEmpty() || _stdNumberStr.isEmpty() || _passwordStr.isEmpty() || _repasswordStr.isEmpty()){
+                    Toast.makeText(getActivity(), "Can't be blank", Toast.LENGTH_LONG).show();
+                    Log.e("REGISTER", "Can't be blank.");
+                }
+                else if (_passwordStr.equals(_repasswordStr)){
+                    Toast.makeText(getActivity(), "Password and Re-Password not equal.", Toast.LENGTH_LONG).show();
+                    Log.e("REGISTER", "Not equal.");
+                }
+                else if (_passwordStr.length() > 6){
+                    Toast.makeText(getActivity(), "Please fill Password at least 6 or more.", Toast.LENGTH_LONG).show();
+                    Log.e("REGISTER", "Password at least 6 or more.");
+                }
+                else{
+                    fbAuth.createUserWithEmailAndPassword(_emailStr, _passwordStr).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            sendVerifyEmail(authResult.getUser());
+                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new LoginFragment()).addToBackStack(null).commit();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.e("REGISTER", "Register fail.");
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void sendVerifyEmail(FirebaseUser _email){
+        _email.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) { }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) { }
+        });
     }
 }
