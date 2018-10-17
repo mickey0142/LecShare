2eaffe369d26f4b977b4c88cc14ece178097c4c9
mickey0142 <59070142@it.kmitl.ac.th>
2018-10-17 10:12:12 +0700

    add progress bar when downloading file add dependency for algolia

--- app/src/main/java/com/kairanpa/se/lecshare/FileListDownloadAdapter.java
+++ app/src/main/java/com/kairanpa/se/lecshare/FileListDownloadAdapter.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +18,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
@@ -37,15 +39,19 @@ public class FileListDownloadAdapter extends ArrayAdapter {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View fileItem = LayoutInflater.from(context).inflate(R.layout.fragment_file_download_list_item, parent, false);
+        final View fileItem = LayoutInflater.from(context).inflate(R.layout.fragment_file_download_list_item, parent, false);
         TextView fileName = fileItem.findViewById(R.id.file_download_list_item_name);
         ImageView downloadButton = fileItem.findViewById(R.id.file_list_item_download_button);
+        final ProgressBar progressBar = fileItem.findViewById(R.id.file_list_item_progress_bar);
+        final ImageView downloadCompleted = fileItem.findViewById(R.id.file_list_item_download_completed);
         final int pos = position;
         downloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("test", "fileList.get(pos) : " + fileList.get(pos));
                 Toast.makeText(context, "downloading " + fileList.get(pos), Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.VISIBLE);
+                downloadCompleted.setVisibility(View.INVISIBLE);
                 try
                 {
                     // add asking for permission somewhere around here
@@ -70,6 +76,18 @@ public class FileListDownloadAdapter extends ArrayAdapter {
                         public void onFailure(@NonNull Exception e) {
                             Log.d("test", "download failed");
                             Toast.makeText(context, "download error : " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnProgressListener(new OnProgressListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                            int progressInt = (int) progress;
+                            progressBar.setProgress(progressInt);
+                            if (progressInt == 100)
+                            {
+                                progressBar.setVisibility(View.INVISIBLE);
+                                downloadCompleted.setVisibility(View.VISIBLE);
+                            }
                         }
                     });
                 }
